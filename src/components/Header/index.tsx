f4b596fdaf14149a4df329e6940e7f1448fc459d
Paulo <paulo.ricardo@repassa.com.br>
2022-02-26 11:16:12 -0300

    feat: create header, product list and product list item components

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import { Container } from './styles';
+
+export function Header() {
+  return (
+    <Container>
+      <h1>
+        Thoughtworks Interview
+      </h1>
+      <div>
+        Total Products: 0
+      </div>
+    </Container>
+  )
+}
