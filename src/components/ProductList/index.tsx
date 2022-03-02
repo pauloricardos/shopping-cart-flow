ad964c438ee461fb43c2f82fa5e22d5eba5fd8d3
Paulo <paulo.ricardo@repassa.com.br>
2022-03-01 21:42:57 -0300

    feat(src/components/ProductList/index.tsx): create add/remove product features

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -1,10 +1,64 @@
 import { ProductListItem } from '../ProductListItem';
 
-export function ProductList() {
+import { IProduct } from '../../models/Product';
+
+import { calculateSubtotal } from '../../utils/calculateSubtotal';
+
+interface ProductListProps {
+  products: IProduct[];
+  setProducts: (value: () => IProduct[]) => void;
+}
+
+export function ProductList({
+  products, setProducts
+}: ProductListProps) {
+  const handleAddProductQuantity = (productId: number) => setProducts(() => {
+    const updatedProducts = products.map((product: IProduct) => {
+      if (product.id === productId) {
+        const newQuantity: number = product.quantity + 1;
+
+        return {
+          ...product,
+          quantity: newQuantity,
+          subtotal: calculateSubtotal(product.price, newQuantity),
+        }
+      } else {
+        return product;
+      }
+    });
+
+    return updatedProducts;
+  });
+
+  const handleRemoveProductQuantity = (productId: number) => setProducts(() => {
+    const updatedProducts = products.map((product: IProduct) => {
+      if (product.id === productId) {
+        const newQuantity: number = product.quantity - 1;
+
+        return {
+          ...product,
+          quantity: newQuantity,
+          subtotal: calculateSubtotal(product.price, newQuantity),
+        }
+      } else {
+        return product;
+      }
+    });
+
+    return updatedProducts;
+  });
+
   return (
     <main>
-      product list
-      <ProductListItem />
+      {products.map(product => (
+        <ul key={product.id}>
+          <ProductListItem
+            product={product}
+            handleAddProductQuantity={handleAddProductQuantity}
+            handleRemoveProductQuantity={handleRemoveProductQuantity}
+          />
+        </ul>
+      ))}
     </main>
   )
 }
