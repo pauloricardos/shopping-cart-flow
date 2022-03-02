418269d64adb258d55ea62a8c3d566131fc6aaac
Paulo <paulo.ricardo@repassa.com.br>
2022-03-01 21:43:26 -0300

    feat(src/__mocks__/productList.ts): add an array of products mock

---
+++ src/__mocks__/productList.ts
@@ -0,0 +1,25 @@
+import { IProduct } from '../models/Product';
+
+export const productListMock: IProduct[] = [
+  {
+    id: 1,
+    name: 'product 1',
+    quantity: 1,
+    price: 5,
+    subtotal: 5,
+  },
+  {
+    id: 2,
+    name: 'product 2',
+    quantity: 1,
+    price: 10,
+    subtotal: 10,
+  },
+  {
+    id: 3,
+    name: 'product 3',
+    quantity: 1,
+    price: 3,
+    subtotal: 3,
+  }
+];
