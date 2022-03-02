f508092f45e32db5febbfa0d63f6a3df51102fac
Paulo <paulo.ricardo@repassa.com.br>
2022-03-01 21:44:47 -0300

    feat(src/components/Header/index.tsx): show total price and total products calculated

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,22 @@
+import { currencyFormatter } from '../../utils/currencyFormatter';
+
 import { Container } from './styles';
 
-export function Header() {
+interface HeaderProps {
+  totalProductsPrice: number;
+  totalProducts: number;
+}
+
+export function Header({ totalProductsPrice, totalProducts }: HeaderProps) {
   return (
     <Container>
       <h1>
         Thoughtworks Interview
       </h1>
       <div>
-        Total Products: 0
+        Total Products: {totalProducts}
+        <br />
+        Total Price: {currencyFormatter(totalProductsPrice)}
       </div>
     </Container>
   )
