aae6bee15fd64781bb0ba5f29028b69a47bb8c70
Paulo <paulo.ricardo@repassa.com.br>
2022-03-01 21:39:41 -0300

    feat(src/utils/currencyFormatter.ts): format to brl currency function

---
+++ src/utils/currencyFormatter.ts
@@ -0,0 +1,6 @@
+export const currencyFormatter = (amount: number) => new Intl.NumberFormat('pt-BR', {
+  style: 'currency',
+  currency: 'BRL',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+}).format(amount);
